bd7e3b8709c3281e0228b5538ab71ba71b499420
Zero Liu <zero.liu@gree.net>
2013-06-25 10:36:43 -0700

    Initial Commit

---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Xiyuan Liu on 6/25/13.
+//  Copyright (c) 2013 Xiyuan Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
---
+++ Matchismo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Matchismo
+//
+//  Created by Xiyuan Liu on 6/25/13.
+//  Copyright (c) 2013 Xiyuan Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CardGameAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CardGameAppDelegate class]));
+    }
+}
